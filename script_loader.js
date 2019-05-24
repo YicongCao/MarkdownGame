0344fd541c8e2c136c549ca34936eaae077faaf7
yicongcao <yicongcao@gmail.com>
2019-05-25 01:23:47 +0800

    调整剧情loader的导出方式

--- script_loader.js
+++ script_loader.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const yaml = require('yamljs')
 const path = require('path')
 
-global.script = yaml.parse(fs.readFileSync(path.join(__dirname, 'script.yaml')).toString())
+var script = yaml.parse(fs.readFileSync(path.join(__dirname, 'script.yaml')).toString())
 
-console.log(JSON.stringify(global.script))
+// console.log(JSON.stringify(script))
+
+module.exports = script
